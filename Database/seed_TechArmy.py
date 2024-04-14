2ac3153819e07779f3298dedc4244ddf008b63c0
ranishstha <ranishstha@gmail.com>
2024-04-13 22:08:48 -0400

    added customer data in seed_techarmy

--- Database/seed_TechArmy.py
+++ Database/seed_TechArmy.py
@@ -9,10 +9,12 @@ def seed_database():
     db.product_orders.drop()  # Drop the collection if it already exists
     db.products.drop() # Drop the collection if it already exists
     db.online_orders.drop() # Drop the collection if it already exists
+    db.customers.drop() # Drop the collection if it already exists
 
     db.create_collection('products')  # Create a new collection named 'products'
     db.create_collection('product_orders')  # Create a new collection named 'product_orders'
     db.create_collection('online_orders')  # Create a new collection named 'online_orders'
+    db.create_collection('customers')  # Create a new collection named 'customers'
 
     # Get the directory of the current script
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -26,6 +28,9 @@ def seed_database():
     # Construct the absolute path to online_orders.json
     online_orders_json_path = os.path.join(current_dir, 'online_orders.json')
 
+    # Construct the absolute path to customers.json
+    customers_json_path = os.path.join(current_dir, 'customers.json')
+
     # Open product.json using the absolute path
     with open(product_json_path) as p:
         product_jsondata = json.load(p)
@@ -37,10 +42,15 @@ def seed_database():
     # Open online_orders.json using the absolute path
     with open(online_orders_json_path) as onp:
         online_orders_jsondata = json.load(onp)
+
+    # Open customers.json using the absolute path
+    with open(customers_json_path) as cj:
+        customers_jsondata = json.load(cj)
     
     db.products.insert_many(product_jsondata)
     db.product_orders.insert_many(product_orders_jsondata)
     db.online_orders.insert_many(online_orders_jsondata)
+    db.customers.insert_many(customers_jsondata)
 
     print("Database and collection created successfully.")
 
